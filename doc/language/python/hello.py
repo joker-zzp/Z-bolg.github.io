ea3ad9da11adfca20611a95bcb2dea53ac7d4de5
joker-zzp <joker.zhangzp@gmail.com>
2019-03-18 23:27:14 +0800

    updata

---
+++ doc/language/python/hello.py
@@ -0,0 +1,11 @@
+# /usr/bin/python3
+# -*- encoding: utf-8 -*-
+#
+# @filename      : hello.py
+# @author        : joker-zzp
+# @created       : Mon Mar 18 2019 13:13:18 GMT+0800 (中国标准时间)
+# @last-modified : Mon Mar 18 2019 18:50:20 GMT+0800 (中国标准时间)
+# @description   : test
+#
+
+print("a")
